c462ec453d99494d56da2e9be5d0e89a19fd9d91
Jonathan Besuchet <jobesu14@gmail.com>
2014-11-18 09:59:09 +0300

    python tchatter_server added

---
+++ server/tchatter_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+ 
+class IphoneChat(Protocol):
+    def connectionMade(self):
+        print "a client connected"
+ 
+factory = Factory()
+factory.protocol = IphoneChat
+reactor.listenTCP(80, factory)
+print "Iphone Chat server started"
+reactor.run()
